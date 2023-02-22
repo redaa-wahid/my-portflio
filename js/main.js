b688ba447c2a514309b479e40acb74735643ef9b
redaa-wahid <114341068+redaa-wahid@users.noreply.github.com>
2023-02-23 01:03:32 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ let link = document.querySelectorAll("aside a");
 console.log(link);
 for (let i = 0; i < link.length; i++) {
 link[i].addEventListener("click",()=>{
+         if (window.innerWidth <= 767) {
         as.style.display = "none";
+    }else{
+        as.style.display = "block";
+    }
 });
 }
